49ba369f8f479d7d33c72b21a6ace1025b54dc8e
Long Nguyen <Long.Nguyen@temple.edu>
2020-03-11 19:02:11 -0400

    Creation of server.js, schema.sql, and seed.sql files

---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS emsDB;
+CREATE database emsDB;
+USE emsDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL, 
+    department_id INT(10) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT, 
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL, 
+    role_id INT(10) NOT NULL, 
+    manager_id INT(10) NULL
+);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
---
+++ seed.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (id, name) VALUES (...,...);
+INSERT INTO department (id, name) VALUES (...,...);
+INSERT INTO department (id, name) VALUES (...,...);
+INSERT INTO department (id, name) VALUES (...,...);
+
+INSERT INTO role (id, title, salary, department) VALUES (...,...);
+INSERT INTO role (id, title, salary, department) VALUES (...,...);
+INSERT INTO role (id, title, salary, department) VALUES (...,...);
+INSERT INTO role (id, title, salary, department) VALUES (...,...);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (...,...);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (...,...);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (...,...);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (...,...);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (...,...);
